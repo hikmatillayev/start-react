00ddae4719b8c1d291219da9c2ee969a84ccf6b2
hikmatillayev <hikmatillayevoktam24@gmail.com>
2023-05-13 15:30:11 +0500

    ppexss

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,7 +15,7 @@ const App=()=>{
             { name: 'Ertugrul', viewers: 789, favourite: false, like: false, id: 2 },
             { name: 'Omar', viewers: 1202, favourite: false, like: false, id: 3 },
         ])
-        const [term, setTerm]=useState('')
+        // const [term, setTerm]=useState('')
         const [filter, setFilter]=useState('')
 }
 
